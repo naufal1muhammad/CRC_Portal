2f58ab092187e070b06b6cb535726e18f83d41fd
maxnaufal99 <naufal@innocel.com.my>
2025-12-10 17:10:27 +0800

    UI Polish
    UI Polish

--- CRC.Web/obj/Debug/net8.0/CRC.Web.AssemblyInfo.cs
+++ CRC.Web/obj/Debug/net8.0/CRC.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRC.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46b6630d00a795d717f61f58184596d133fcbde4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c272db85cfb5b37317f833751af98c916429883")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRC.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRC.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
