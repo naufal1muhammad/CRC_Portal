b3b810e50aaa2fef5a22e1147a7503b3886e56a1
maxnaufal99 <naufal@innocel.com.my>
2025-12-22 00:06:46 +0800

    UI Changes
    UI Changes

--- CRC.Web/obj/Debug/net8.0/CRC.Web.AssemblyInfo.cs
+++ CRC.Web/obj/Debug/net8.0/CRC.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRC.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7be36e859812580019108241939741a06d5a1959")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7ff50def46739048d269c21cf32fad01f0fddaf")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRC.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRC.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
