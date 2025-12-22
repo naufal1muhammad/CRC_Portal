41366e68013db8b68ce63e5b5983a2b9b41d8b7b
maxnaufal99 <naufal@innocel.com.my>
2025-12-22 12:54:33 +0800

    Staff Update Bug
    Staff Update Bug

--- CRC.Database/Stored Procedures/Staff/spStaff_Update.sql
+++ CRC.Database/Stored Procedures/Staff/spStaff_Update.sql
@@ -1,4 +1,5 @@
 ﻿CREATE PROCEDURE [dbo].[spStaff_Update]
+(
     @Staff_ID    VARCHAR(100),
     @Staff_Name  VARCHAR(100),
     @Staff_NRIC  VARCHAR(100),
@@ -7,10 +8,14 @@
     @Branch_ID   VARCHAR(100),
     @Branch_Name VARCHAR(100),
     @Staff_Type  VARCHAR(100)  -- still pass it, but UI will keep it fixed
+)
 AS
 BEGIN
     SET NOCOUNT ON;
 
+    ---------------------------------
+    -- 1) Update main Staff
+    ---------------------------------
     UPDATE [dbo].[Staff]
     SET
         [Staff_Name]  = @Staff_Name,
@@ -21,4 +26,30 @@ BEGIN
         [Branch_Name] = @Branch_Name,
         [Staff_Type]  = @Staff_Type
     WHERE [Staff_ID] = @Staff_ID;
+
+    ---------------------------------
+    -- 2) Cascade updates to children
+    ---------------------------------
+
+    -- PatientAppointment: Staff_ID, Staff_Name
+    UPDATE pa
+    SET
+        pa.[Staff_Name] = @Staff_Name
+    FROM [dbo].[PatientAppointment] pa
+    WHERE pa.[Staff_ID] = @Staff_ID;
+
+    -- PatientJourneyAudit: Staff_ID, Staff_Name
+    UPDATE pja
+    SET
+        pja.[Staff_Name] = @Staff_Name
+    FROM [dbo].[PatientJourneyAudit] pja
+    WHERE pja.[Staff_ID] = @Staff_ID;
+
+    -- StaffDocument: Staff_ID, Staff_Name
+    UPDATE sd
+    SET
+        sd.[Staff_Name] = @Staff_Name
+    FROM [dbo].[StaffDocument] sd
+    WHERE sd.[Staff_ID] = @Staff_ID;
 END;
+GO
