ab3875f2c447ab73f50e34037bc973b23e808ca8
maxnaufal99 <naufal@innocel.com.my>
2025-12-25 11:20:52 +0800

    ENH - Edit sidebar menu and login page with new logo
    ENH - Edit sidebar menu and login page with new logo

--- CRC.Web/obj/Debug/net8.0/CRC.Web.AssemblyInfo.cs
+++ CRC.Web/obj/Debug/net8.0/CRC.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRC.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec04b10cb1295ea6b1ccf4c80c8858cbef9f22b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47c4a62ad8cb24c1bed357378c2bba571d7c3c45")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRC.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRC.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
