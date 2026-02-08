4afa66abf9bea73bfcee05885b00f7416fd7343c
naufal1muhammad <76731401+naufal1muhammad@users.noreply.github.com>
2026-02-08 13:17:08 +0800

    Update staff patient basic details view

--- CRC.Database/Stored Procedures/PatientBasic/spPatientBasic_GetById.sql
+++ CRC.Database/Stored Procedures/PatientBasic/spPatientBasic_GetById.sql
@@ -6,22 +6,12 @@ AS
 BEGIN
     SET NOCOUNT ON;
 
-    ;WITH FirstAppt AS
-    (
-        SELECT pa.Patient_ID,
-               MIN(pa.PatientAppointment_Date) AS FirstAppointmentDate
-        FROM dbo.PatientAppointment pa
-        WHERE pa.Patient_ID = @Patient_ID
-        GROUP BY pa.Patient_ID
-    )
     SELECT
         pb.[Patient_ID],
         pb.[Patient_Name],
         pb.[Patient_Email],
         pb.[Patient_Phone],
         pb.[Patient_NRIC],
-
-        fa.FirstAppointmentDate AS [Patient_AdmittedOn],
 
         pb.[Patient_BirthDate],
         pb.[Patient_Age],
@@ -43,41 +33,15 @@ BEGIN
         pb.[Patient_AddLine1],
         pb.[Patient_AddLine2],
 
-        -- Backward compatible computed address (old UI/staff view)
-        (pb.[Patient_AddLine1]
-            + CASE WHEN pb.[Patient_AddLine2] IS NOT NULL AND LTRIM(RTRIM(pb.[Patient_AddLine2])) <> '' THEN CHAR(10) + pb.[Patient_AddLine2] ELSE '' END
-            + CASE WHEN ISNULL(pb.[Patient_ResPostcode], '') <> '' THEN CHAR(10) + pb.[Patient_ResPostcode] ELSE '' END
-            + CASE WHEN ISNULL(pb.[Patient_ResCity], '') <> '' THEN ' ' + pb.[Patient_ResCity] ELSE '' END
-            + CASE WHEN ISNULL(pb.[Patient_ResState], '') <> '' THEN ', ' + pb.[Patient_ResState] ELSE '' END
-        ) AS [Patient_Address],
-
         pb.[Patient_EmergencyName],
         pb.[Patient_EmergencyRelationship],
-        pb.[Patient_EmergencyNumber],
-
-        -- Backward compatible derived branch name (from latest appointment)
-        br.[Branch_Name],
-
-        pb.[DischargeType_ID],
-        dt.[DischargeType_Name],
-        pb.[Patient_DischargeDate],
-        pb.[Patient_DischargeRemarks]
+        pb.[Patient_EmergencyNumber]
     FROM dbo.PatientBasic pb
     LEFT JOIN dbo.LU_RACE r              ON r.Race_ID = pb.Race_ID
     LEFT JOIN dbo.LU_SOURCE s            ON s.Source_ID = pb.Source_ID
     LEFT JOIN dbo.LU_RELIGION rel        ON rel.Religion_ID = pb.Religion_ID
     LEFT JOIN dbo.LU_MARITALSTATUS ms    ON ms.MaritalStatus_ID = pb.MaritalStatus_ID
     LEFT JOIN dbo.LU_OCCUPATION occ      ON occ.Occupation_ID = pb.Occupation_ID
-    LEFT JOIN dbo.LU_DISCHARGETYPE dt    ON dt.DischargeType_ID = pb.DischargeType_ID
-    LEFT JOIN FirstAppt fa               ON fa.Patient_ID = pb.Patient_ID
-    OUTER APPLY
-    (
-        SELECT TOP 1 pa.Branch_ID
-        FROM dbo.PatientAppointment pa
-        WHERE pa.Patient_ID = pb.Patient_ID
-        ORDER BY pa.PatientAppointment_Date DESC, pa.PatientAppointment_ID DESC
-    ) lastAppt
-    LEFT JOIN dbo.Branch br ON br.Branch_ID = lastAppt.Branch_ID
     WHERE pb.[Patient_ID] = @Patient_ID;
 END;
 GO
