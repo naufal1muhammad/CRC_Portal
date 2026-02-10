fe5333d07a0b3742ad2b7a87d39af83ee7eda704
maxnaufal99 <naufal@innocel.com.my>
2026-02-10 12:24:47 +0800

    whatever
    whatever

--- CRC.Web/obj/Debug/net8.0/CRC.Web.AssemblyInfo.cs
+++ CRC.Web/obj/Debug/net8.0/CRC.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRC.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98c7daa9402ddf0da596279b62d142544ffbf43e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a9b5e3cf5ad5c6e23b9b10d44c1e50355029cab")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRC.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRC.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
