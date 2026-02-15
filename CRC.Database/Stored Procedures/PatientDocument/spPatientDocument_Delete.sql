fdc5ec04171c4688a64923cde8d2b6d96a328824
maxnaufal99 <naufal@innocel.com.my>
2026-02-15 14:51:13 +0800

    Audit Trails for PatientDocument
    Audit Trails for PatientDocument

--- CRC.Database/Stored Procedures/PatientDocument/spPatientDocument_Delete.sql
+++ CRC.Database/Stored Procedures/PatientDocument/spPatientDocument_Delete.sql
@@ -1,12 +1,76 @@
 ﻿CREATE PROCEDURE [dbo].[spPatientDocument_Delete]
 (
-    @PatientDocument_ID INT
+    @PatientDocument_ID INT,
+    @User_ID            INT = NULL
 )
 AS
 BEGIN
     SET NOCOUNT ON;
 
+    -- Capture details for audit (best effort)
+    DECLARE
+        @Patient_ID VARCHAR(100),
+        @Patient_Name VARCHAR(100),
+        @PatientDocumentType_ID VARCHAR(100),
+        @PatientDocumentType_Name VARCHAR(100),
+        @FileName VARCHAR(255),
+        @FilePath VARCHAR(500),
+        @ContentType VARCHAR(100),
+        @UploadedOn VARCHAR(100);
+
+    SELECT
+        @Patient_ID = [Patient_ID],
+        @PatientDocumentType_ID = [PatientDocumentType_ID],
+        @FileName = [FileName],
+        @FilePath = [FilePath],
+        @ContentType = [ContentType],
+        @UploadedOn = [UploadedOn]
+    FROM [dbo].[PatientDocument]
+    WHERE [PatientDocument_ID] = @PatientDocument_ID;
+
+    -- Lookups (best effort)
+    SELECT
+        @Patient_Name = [Patient_Name]
+    FROM [dbo].[PatientBasic]
+    WHERE [Patient_ID] = @Patient_ID;
+
+    SELECT
+        @PatientDocumentType_Name = [PatientDocumentType_Name]
+    FROM [dbo].[LU_PATDOCUMENTTYPE]
+    WHERE [PatientDocumentType_ID] = @PatientDocumentType_ID;
+
     DELETE FROM [dbo].[PatientDocument]
     WHERE [PatientDocument_ID] = @PatientDocument_ID;
+
+    DECLARE @RowsAffected INT = @@ROWCOUNT;
+
+    IF @RowsAffected > 0
+    BEGIN
+        -- -----------------------------
+        -- Audit trail
+        -- -----------------------------
+        INSERT INTO [dbo].[AuditTrails]
+        (
+            [User_Id],
+            [AuditTrail_Action],
+            [AuditTrail_Category],
+            [AuditTrail_Summary]
+        )
+        VALUES
+        (
+            ISNULL(@User_ID, 0),
+            'DELETE',
+            'PatientDocument',
+            CONCAT(
+                'Deleted Patient Document: PatientDocument_ID=', @PatientDocument_ID,
+                '; Patient_ID=', ISNULL(@Patient_ID, ''),
+                '; Patient_Name=', ISNULL(@Patient_Name, ''),
+                '; DocType=', ISNULL(@PatientDocumentType_Name, ''), ' (', ISNULL(@PatientDocumentType_ID, ''), ')',
+                '; FileName=', ISNULL(@FileName, ''),
+                '; ContentType=', ISNULL(@ContentType, ''),
+                '; UploadedOn=', ISNULL(@UploadedOn, '')
+            )
+        );
+    END
 END;
 GO
--- CRC.Database/Stored Procedures/PatientDocument/spPatientDocument_Insert.sql
+++ CRC.Database/Stored Procedures/PatientDocument/spPatientDocument_Insert.sql
@@ -6,7 +6,8 @@ CREATE PROCEDURE [dbo].[spPatientDocument_Insert]
     @PatientDocumentType_Name VARCHAR(100),
     @FileName                 VARCHAR(255),
     @FilePath                 VARCHAR(500),
-    @ContentType              VARCHAR(100)
+    @ContentType              VARCHAR(100),
+    @User_ID                  INT = NULL
 )
 AS
 BEGIN
@@ -29,6 +30,33 @@ BEGIN
         @FilePath,
         @ContentType,
         CONVERT(VARCHAR(100), GETDATE(), 120)
+    );
+
+    DECLARE @InsertedPatientDocumentId INT = CONVERT(INT, SCOPE_IDENTITY());
+
+    -- -----------------------------
+    -- Audit trail
+    -- -----------------------------
+    INSERT INTO [dbo].[AuditTrails]
+    (
+        [User_Id],
+        [AuditTrail_Action],
+        [AuditTrail_Category],
+        [AuditTrail_Summary]
+    )
+    VALUES
+    (
+        ISNULL(@User_ID, 0),
+        'INSERT',
+        'PatientDocument',
+        CONCAT(
+            'Uploaded Patient Document: PatientDocument_ID=', @InsertedPatientDocumentId,
+            '; Patient_ID=', @Patient_ID,
+            '; Patient_Name=', ISNULL(@Patient_Name, ''),
+            '; DocType=', ISNULL(@PatientDocumentType_Name, ''), ' (', ISNULL(@PatientDocumentType_ID, ''), ')',
+            '; FileName=', @FileName,
+            '; ContentType=', @ContentType
+        )
     );
 END;
 GO
