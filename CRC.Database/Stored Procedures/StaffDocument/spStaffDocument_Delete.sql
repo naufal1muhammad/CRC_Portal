15237a00585082930d1ae05bb217c6a1aea2bb88
naufal1muhammad <76731401+naufal1muhammad@users.noreply.github.com>
2026-02-15 16:33:41 +0800

    Add audit trail logging to StaffDocument insert and delete sprocs

--- CRC.Database/Stored Procedures/StaffDocument/spStaffDocument_Delete.sql
+++ CRC.Database/Stored Procedures/StaffDocument/spStaffDocument_Delete.sql
@@ -1,9 +1,76 @@
-﻿CREATE PROCEDURE [dbo].[spStaffDocument_Delete]
-    @StaffDocument_ID INT
+CREATE PROCEDURE [dbo].[spStaffDocument_Delete]
+(
+    @StaffDocument_ID INT,
+    @User_ID          INT = NULL
+)
 AS
 BEGIN
     SET NOCOUNT ON;
 
+    -- Capture details for audit (best effort)
+    DECLARE
+        @Staff_ID VARCHAR(100),
+        @Staff_Name VARCHAR(100),
+        @StaffDocumentType_ID VARCHAR(100),
+        @StaffDocumentType_Name VARCHAR(100),
+        @FileName VARCHAR(255),
+        @FilePath VARCHAR(500),
+        @ContentType VARCHAR(100),
+        @UploadedOn VARCHAR(100);
+
+    SELECT
+        @Staff_ID = [Staff_ID],
+        @StaffDocumentType_ID = [StaffDocumentType_ID],
+        @FileName = [FileName],
+        @FilePath = [FilePath],
+        @ContentType = [ContentType],
+        @UploadedOn = CONVERT(VARCHAR(100), [UploadedOn], 120)
+    FROM [dbo].[StaffDocument]
+    WHERE [StaffDocument_ID] = @StaffDocument_ID;
+
+    -- Lookups (best effort)
+    SELECT
+        @Staff_Name = [Staff_Name]
+    FROM [dbo].[Staff]
+    WHERE [Staff_ID] = @Staff_ID;
+
+    SELECT
+        @StaffDocumentType_Name = [StaffDocumentType_Name]
+    FROM [dbo].[LU_STAFFDOCUMENTTYPE]
+    WHERE [StaffDocumentType_ID] = @StaffDocumentType_ID;
+
     DELETE FROM [dbo].[StaffDocument]
     WHERE [StaffDocument_ID] = @StaffDocument_ID;
+
+    DECLARE @RowsAffected INT = @@ROWCOUNT;
+
+    IF @RowsAffected > 0
+    BEGIN
+        -- -----------------------------
+        -- Audit trail
+        -- -----------------------------
+        INSERT INTO [dbo].[AuditTrails]
+        (
+            [User_Id],
+            [AuditTrail_Action],
+            [AuditTrail_Category],
+            [AuditTrail_Summary]
+        )
+        VALUES
+        (
+            ISNULL(@User_ID, 0),
+            'DELETE',
+            'StaffDocument',
+            CONCAT(
+                'Deleted Staff Document: StaffDocument_ID=', @StaffDocument_ID,
+                '; Staff_ID=', ISNULL(@Staff_ID, ''),
+                '; Staff_Name=', ISNULL(@Staff_Name, ''),
+                '; DocType=', ISNULL(@StaffDocumentType_Name, ''), ' (', ISNULL(@StaffDocumentType_ID, ''), ')',
+                '; FileName=', ISNULL(@FileName, ''),
+                '; ContentType=', ISNULL(@ContentType, ''),
+                '; UploadedOn=', ISNULL(@UploadedOn, '')
+            )
+        );
+    END
 END;
+GO
--- CRC.Database/Stored Procedures/StaffDocument/spStaffDocument_Insert.sql
+++ CRC.Database/Stored Procedures/StaffDocument/spStaffDocument_Insert.sql
@@ -1,11 +1,14 @@
-﻿CREATE PROCEDURE [dbo].[spStaffDocument_Insert]
+CREATE PROCEDURE [dbo].[spStaffDocument_Insert]
+(
     @Staff_ID               VARCHAR(100),
     @Staff_Name             VARCHAR(100),
     @StaffDocumentType_ID   VARCHAR(100),
     @StaffDocumentType_Name VARCHAR(100),
     @FileName               VARCHAR(255),
     @FilePath               VARCHAR(500),
-    @ContentType            VARCHAR(100)
+    @ContentType            VARCHAR(100),
+    @User_ID                INT = NULL
+)
 AS
 BEGIN
     SET NOCOUNT ON;
@@ -28,4 +31,32 @@ BEGIN
         @ContentType,
         GETDATE()
     );
+
+    DECLARE @InsertedStaffDocumentId INT = CONVERT(INT, SCOPE_IDENTITY());
+
+    -- -----------------------------
+    -- Audit trail
+    -- -----------------------------
+    INSERT INTO [dbo].[AuditTrails]
+    (
+        [User_Id],
+        [AuditTrail_Action],
+        [AuditTrail_Category],
+        [AuditTrail_Summary]
+    )
+    VALUES
+    (
+        ISNULL(@User_ID, 0),
+        'INSERT',
+        'StaffDocument',
+        CONCAT(
+            'Uploaded Staff Document: StaffDocument_ID=', @InsertedStaffDocumentId,
+            '; Staff_ID=', @Staff_ID,
+            '; Staff_Name=', ISNULL(@Staff_Name, ''),
+            '; DocType=', ISNULL(@StaffDocumentType_Name, ''), ' (', ISNULL(@StaffDocumentType_ID, ''), ')',
+            '; FileName=', @FileName,
+            '; ContentType=', @ContentType
+        )
+    );
 END;
+GO
