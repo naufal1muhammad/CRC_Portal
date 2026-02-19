72df0bb46dbc2118c81878f376d57b21ea99dc8d
maxnaufal99 <naufal@innocel.com.my>
2026-02-19 13:05:43 +0800

    UI Changes on Login page
    UI Changes on Login page

--- CRC.Web/obj/Debug/net8.0/CRC.Web.AssemblyInfo.cs
+++ CRC.Web/obj/Debug/net8.0/CRC.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRC.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+996c491607f3da2ca9be182d946a894471a414e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8148d63d88ed8e98c5a368eb7c03df21e0354ea5")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRC.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRC.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
