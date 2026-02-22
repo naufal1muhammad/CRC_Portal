f62b79419b35ce1335efbd7b34ec70c638cef549
maxnaufal99 <naufal@innocel.com.my>
2026-02-22 10:10:47 +0800

    Staff Document deletion
    Staff Document deletion

--- CRC.Web/obj/Debug/net8.0/CRC.Web.AssemblyInfo.cs
+++ CRC.Web/obj/Debug/net8.0/CRC.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRC.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a87e05d7f2aaeeeb1d2247df6b1bfccf78b2bb88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cacb30738444c8ccda28ca87074e3f36886c3d49")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRC.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRC.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
