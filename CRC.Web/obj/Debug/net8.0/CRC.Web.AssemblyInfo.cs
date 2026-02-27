3705401d1c938133338cf0e5d35b399341b20910
maxnaufal99 <naufal@innocel.com.my>
2026-02-27 15:19:18 +0800

    Cache changes
    Cache changes

--- CRC.Web/obj/Debug/net8.0/CRC.Web.AssemblyInfo.cs
+++ CRC.Web/obj/Debug/net8.0/CRC.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRC.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfd163bc5ec3cf5dc75c11728bc28228fec1ff15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fef0e735a5aeddae7a70d873e7b8564e8ef76855")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRC.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRC.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
