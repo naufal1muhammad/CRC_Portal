4a52d2f4e518e767a806503686635640ac8d514a
maxnaufal99 <naufal@innocel.com.my>
2026-04-15 10:48:40 +0800

    Timeout change from 1minute to 10 minutes
    Timeout change from 1 minute to 10 minutes

--- CRC.Web/Models/SessionTimeoutOptions.cs
+++ CRC.Web/Models/SessionTimeoutOptions.cs
@@ -2,6 +2,6 @@ namespace CRC.Web.Models
 {
     public class SessionTimeoutOptions
     {
-        public int InactivityTimeoutSeconds { get; set; } = 60;
+        public int InactivityTimeoutSeconds { get; set; } = 600;
     }
 }
