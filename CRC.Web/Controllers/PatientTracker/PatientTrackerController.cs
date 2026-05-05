6902bc663176105b73f44b713cd6573493cbc6a2
maxnaufal99 <naufal@innocel.com.my>
2026-05-05 14:14:58 +0800

    Files creation for feature
    Files creation for feature

---
+++ CRC.Web/Controllers/PatientTracker/PatientTrackerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRC.Web.Controllers.PatientTracker
+{
+    public class PatientTrackerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
